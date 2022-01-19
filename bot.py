b626003423b88035106e705d83ae6b6e41e87ceb
Colton Nguyen <colt@colty-mac-mini.local>
2022-01-19 16:46:28 -0700

    structure change

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
 
-Bot = commands.Bot(command_prefix='gay:')
+#imports cogs
+from main_cog import main_cog
+from music_cog import music_cog
 
-@Bot.command()
-async def echo(ctx, *args):
-  m_args = " ".join(args)
-  await ctx.send(m_args)
+Bot = commands.Bot(command_prefix="gay:")
+
+Bot.add_cog(main_cog(Bot))
+Bot.add_cog(music_cog(Bot))
 
 token = ""
 with open("token.txt") as file:
